8a7a6929096d4ea971dabffce4a47c9c1fdb37ed
animesh65432 <kiranduttta234@gmail.com>
2024-02-17 11:41:50 +0530

    Integrating it with firebase

--- src/Components/Expense/AddExpenses.jsx
+++ src/Components/Expense/AddExpenses.jsx
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./expense.css";
+import axios from "axios";
 
 const AddExpenses = () => {
+  const Moneyref = useRef();
+  const Descriptionref = useRef();
+  const Catetgoryref = useRef();
+  const Dateref = useRef();
+
+  const OnsumbitAddexpenses = async () => {
+    let Usermoney = Moneyref.current.value;
+    let Description = Descriptionref.current.value;
+    let Category = Catetgoryref.current.value;
+    let Date = Dateref.current.value;
+
+    try {
+      let reponse = await axios.post(
+        `https://sgarpner-project-default-rtdb.firebaseio.com/Save.json`,
+        {
+          money: Usermoney,
+          Description: Description,
+          Category: Category,
+          Date: Date
+        }
+      );
+      console.log(reponse);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div id="Container">
       <div id="addExpensesContainer" className="expenses-container">
         <div className="input-group">
           <label htmlFor="spentMoney">Spent Money</label>
-          <input type="text" id="spentMoney" />
+          <input type="text" id="spentMoney" ref={Moneyref} />
         </div>
         <div className="input-group">
           <label htmlFor="description">Description</label>
-          <input type="text" id="description" />
+          <input type="text" id="description" ref={Descriptionref} />
         </div>
         <div className="input-group">
           <label htmlFor="category">Category</label>
-          <select id="category">
+          <select id="category" ref={Catetgoryref}>
             <option value="Food">Food</option>
             <option value="Transportation">Transportation</option>
             <option value="Utilities">Utilities</option>
@@ -25,9 +52,9 @@ const AddExpenses = () => {
         </div>
         <div className="input-group">
           <label htmlFor="date">Date</label>
-          <input type="date" id="date" />
+          <input type="date" id="date" ref={Dateref} />
         </div>
-        <button>Add Expenses</button>
+        <button onClick={OnsumbitAddexpenses}>Add Expenses</button>
       </div>
     </div>
   );
